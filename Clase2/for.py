d9b8d0277768744e5a14061460520947ce665c0b
ozzvb <osvaldovenegas@gmail.com>
2022-02-24 21:07:23 -0600

    24022022_2106

--- Clase2/for.py
+++ Clase2/for.py
@@ -12,7 +12,6 @@ lista=[ 'casa', 'carro', 'cancho', 30 ]
 for x in range(len(lista)) :
     print("El elemento ", x , " de la lista es: ", lista[x])
 
-
 palabra="Python"
 for x in range( len(palabra) ) :
     print( "La letra ", x , "de la palabra es", palabra[x] )
