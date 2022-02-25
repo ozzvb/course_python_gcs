5d3818b80721f30a2e796c57f8b2aa486f51ff3c
ozzvb <osvaldovenegas@gmail.com>
2022-02-24 20:46:38 -0600

    24022022_2046

--- Clase2/for.py
+++ Clase2/for.py
@@ -1,6 +1,19 @@
 for num in range(101) :
-    if num == 100 :
-        print(num)
-    else :
-        print(num,end=" - ")
+    if num != 0 :
+        if num == 100 :
+            print(num)
+        else :
+            print(num,end=" - ")
+            
+print ("----------------------------------")
 
+lista=[ 'casa', 'carro', 'cancho', 30 ]
+
+for x in range(len(lista)) :
+    print("El elemento ", x , " de la lista es: ", lista[x])
+
+
+palabra="Python"
+for x in range( len(palabra) ) :
+    print( "La letra ", x , "de la palabra es", palabra[x] )
+    
