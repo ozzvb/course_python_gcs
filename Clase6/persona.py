96ef6a3eec5a5c17a7e79feeaaa5b78efcccbdae
osvaldobvenegas@gmail.com <osvaldovenegas@gmail.com>
2022-03-10 18:49:29 -0600

    Cambios

---
+++ Clase6/persona.py
@@ -0,0 +1,23 @@
+class Persona :
+    color_pelo = "negro"
+    color_ojos = "negro"
+    
+print( type(Persona) )
+
+osvaldo=Persona();
+osvaldo.color_ojos="cafe"
+osvaldo.color_pelo="castaño"
+
+print( osvaldo.color_pelo )
+
+
+#////////////////////////////////////////////////////////////////
+
+class Persona2 :
+    def __init__( self, color_pelo= "rubio", color_ojos = "azul" ) :
+        self.color_pelo = color_pelo
+        self.color_ojos = color_ojos
+        
+mario=Persona2("rubio","cafe");
+luciana=Persona2("negro","verdes");
+
